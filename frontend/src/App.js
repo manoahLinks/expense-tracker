42d0b791a48fd03b660bb40bbd36c2ad7537497a
manoahLinks <manoahluka@gmail.com>
2022-11-10 14:29:49 +0100

    initializing the front and backend of folders with files

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div className='bg-red-800'>Manoah</div>
+      </div>
+    );
+  }
+}
+
+export default App;
