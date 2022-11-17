f9cb53f6d6bcf674aec38381a514124e9a6e0096
manoahLinks <manoahluka@gmail.com>
2022-11-17 08:40:46 +0100

    including react router dom to handle routing of pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,28 @@
 import React, {Component} from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import NavBar from './components/NavBar';
 import TaskBar from './components/TaskBar';
+import HomePage from './pages/HomePage';
 import Transactions from './components/Transactions';
+import ExpenseForm from './components/ExpenseForm';
 
 class App extends Component {
   render() {
     return (
-      <div className="grid grid-cols-1 w-full relative text-gray-700 text-xs md:text-sm">
+      <Router>
+        <div className="grid grid-cols-1 w-full relative text-gray-700 text-xs md:text-sm">
           <NavBar />
           <div className='flex flex-col md:flex-row w-full'>
-            <Transactions />
+            <Routes>
+              <Route exact path={`/`} element={<HomePage></HomePage>}></Route>
+              <Route path={`/transactions`} element={<Transactions></Transactions>}></Route>
+              <Route path={`/new`} element={<ExpenseForm></ExpenseForm>}></Route>
+            </Routes>
             <TaskBar />
           </div>
-      </div>
+        </div>
+      </Router>
+      
     );
   }
 }
