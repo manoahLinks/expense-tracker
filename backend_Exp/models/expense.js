270b7caed3362c7609a95c54c3fb93696ba92cb3
Semmwa <bakshaksemmwa@gmail.com>
2022-11-18 02:36:59 +0100

    folder name modified

--- backend_Exp/models/expense.js
+++ backend_Exp/models/expense.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose')
 
 
-// database schema
-
+//setting up database schema
 const expenseSchema = new mongoose.Schema({
     title : {type:String, required:true},
-    amount : {type: Number, required: true}
-    // created_at: {type: Date, default: Date.now()}
+    amount : {type: Number, required: true},
+    disburse: {type : Boolean, default:false}
 },{timestamps:true})
+
 
 // creating a model
 Expense = mongoose.model('Expense', expenseSchema)
