88cb3918cb0f9beb119cd080cfcab021f9801ba4
Semmwa <bakshaksemmwa@gmail.com>
2022-11-23 23:23:25 +0100

    modified database schema

--- backend_Exp/models/expense.js
+++ backend_Exp/models/expense.js
@@ -5,6 +5,7 @@ const mongoose = require('mongoose')
 const expenseSchema = new mongoose.Schema({
     title : {type:String, required:true},
     amount : {type: Number, required: true},
+    description : {type: String, required: true},
     disburse: {type : Boolean, default:false}
 },{timestamps:true})
 
